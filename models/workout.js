08ab6318afef86cc924361d1fa1036ccbe6ef59e
maxmay94 <maxmay94@gmail.com>
2022-10-18 15:21:34 -0400

    workout model added

---
+++ models/workout.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: { type: String },
+    exercises: [{type: Schema.Types.ObjectId, ref: 'Exercise'}],
+    setsReps: { type: [String] }
+  },
+  { timestamps: true }
+)
+
+const workout = mongoose.model('Workout', workoutSchema)
+
+export { workout }
