32a61747527b7bde99b139a958b49888075d5eac
maxmay94 <maxmay94@gmail.com>
2022-10-18 15:14:59 -0400

    add exercise model

---
+++ models/exercise.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const exerciseSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    description: { type: String },
+    muscle: {
+      type: String,
+      enum:[
+        'chest',
+        'back',
+        'biceps',
+        'triceps',
+        'abs',
+        'legs',
+        'quads',
+        'hamstrings',
+        'arms',
+        'shoulders',
+        'obliques',
+        'traps',
+        'lats',
+        'delts',
+        'cardio',
+        'full body'
+      ]
+    },
+    url: { type: String }
+  },
+  { timestamps: true }
+)
+
+const exercise = mongoose.model('Exercise', exerciseSchema)
+
+export { exercise }
