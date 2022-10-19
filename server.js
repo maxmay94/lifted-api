b4ea41c941c18ecefd21f409bf84add3381cf5f4
maxmay94 <maxmay94@gmail.com>
2022-10-19 11:12:26 -0400

    mount router to server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import formData from 'express-form-data'
 
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as exerciseRouter } from './routes/exercises.js'
 
 import './config/database.js'
 
@@ -18,6 +19,8 @@ app.use(formData.parse())
 
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/exercises', exerciseRouter)
+
 
 app.use(function (req, res, next) {
   res.status(404).json({ err: 'Not found' })
