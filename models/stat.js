fd2fa9cbe852c91b1d029eb2616a86dd324132ea
maxmay94 <maxmay94@gmail.com>
2022-10-20 10:25:25 -0400

    change tracker to stat model and update fields

---
+++ models/stat.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const statSchema = new Schema(
+  {
+    date: { type: Date },
+    exercise: {
+      type: Schema.Types.ObjectId, 
+      ref: 'Exercise'
+    },
+    reps: [Int],
+    weightsUsed: [String],
+    user: {
+      type: Schema.Types.ObjectId, 
+      ref: 'Profile'
+    }
+  },
+  { timestamps: true }
+)
+
+const Stat = mongoose.model('Stat', statSchema)
+
+export { Stat }
